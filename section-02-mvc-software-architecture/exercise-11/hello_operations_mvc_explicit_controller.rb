02e4ebeddbc5982c23af22f1a1e45e979967fca5
Andy Maleh <andy.am@gmail.com>
2023-11-10 14:36:09 -0500

    Move exercises into sections and refactor exercise 11

--- section-02-mvc-software-architecture/exercise-11/hello_operations_mvc_explicit_controller.rb
+++ section-02-mvc-software-architecture/exercise-11/hello_operations_mvc_explicit_controller.rb
@@ -8,14 +8,6 @@ class OptionModel
     @selected_options = []
   end
   
-  def to_s
-    if @selected_options.empty?
-      'None'
-    else
-      @selected_options.sort.map { |option_number| "Option #{option_number}" }.join(', ')
-    end
-  end
-  
   def toggle_option(option_number)
     if @selected_options.include?(option_number)
       @selected_options.delete(option_number)
@@ -23,13 +15,34 @@ class OptionModel
       @selected_options << option_number
     end
   end
+  
+  def to_s
+    if @selected_options.empty?
+      'None'
+    else
+      @selected_options.sort.map { |option_number| "Option #{option_number}" }.join(', ')
+    end
+  end
+end
+
+class OptionController
+  attr_reader :option_model
+    
+  def initialize
+    @option_model = OptionModel.new
+  end
+  
+  def toggle_option(option_number)
+    @option_model.toggle_option(option_number)
+  end
 end
 
 class OptionView
   include Glimmer
 
   def initialize
-    @option_model = OptionModel.new
+    @option_controller = OptionController.new
+    @option_model = @option_controller.option_model
     create_window
     register_observers
   end
@@ -47,7 +60,7 @@ class OptionView
             option_number = n + 1
             checkbox("Option #{option_number}") {
               on_toggled do
-                @option_model.toggle_option(option_number)
+                @option_controller.toggle_option(option_number)
               end
             }
           end
